976aadfee76597a6af84c5177d8aba22a5483248
VaneQiuta777 <vanessa.colsan@gmail.com>
2018-01-03 16:02:30 -0500

    añadiendo correcciones, luego del code review

--- js/form.js
+++ js/form.js
@@ -2,12 +2,6 @@ $(document).ready(function () {
   var $firstName = $('#first-name');
   var $lastName = $('#last-name');
   var $email = $('#email');
-  // habilitar el botón-next
-  $('#check').click(function () {
-    console.log('sdsa')
-    $('#submit').removeAttr('disabled');
-
-  });
   // validar que ingrese sólo letras
   $(':text#first-name').on('input', function () {
     if ($(this).val().match(/[1-9]/)) {
@@ -21,18 +15,24 @@ $(document).ready(function () {
       $(this).val('');
     }
   });
-  //validar el email
+  // validar el email
   $email.on('input', function (event) {
     // console.log(event.target.value);
     var REGEXEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
     console.log(REGEXEMAIL.test($(this).val()));
     console.log($(this).val());
     if (REGEXEMAIL.test($(this).val())) {
-      console.log($(this).val()); 
-    } 
+      console.log($(this).val());
+    }
   });
-   //redireccionar a la última vista --> inconvenientes por default
-   $('#submit').click(function () {
+  // habilitar el botón-next
+  $('#check').click(function () {
+    console.log('sdsa')
+    $('#submit').removeAttr('disabled');
+
+  });
+  // redireccionar a la última vista
+  $('#submit').click(function () {
     if ($('#check').prop('checked') && $('#first-name').val() && $('#last-name').val() && $('#email').val()) {
       event.preventDefault();
       window.location.href = '../views/ultimate.html';
